ca7d03374a853f823af7b177c8717cac65fe3815
Yuanhao-Luo <1097308076@qq.com>
2022-11-05 16:12:44 +0000

    test company page

---
+++ movies/cypress/e2e/company.cy.js
@@ -0,0 +1,40 @@
+let movies;
+let movieId; // Enola Holmes movie id
+let movie
+
+describe("Company", () => {
+    before(() => {
+        cy.request(
+                `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+            )
+            .its("body")
+            .then((response) => {
+                movies = response.results;
+            });
+    });
+    beforeEach(() => {
+        cy.visit("/");
+    });
+    describe("From a movie detail to a production company detail", () => {
+        before(() => {
+            cy.request(
+                    `https://api.themoviedb.org/3/movie/${movies[1].id}?api_key=${Cypress.env("TMDB_KEY")}`
+                )
+                .its("body")
+                .then((movieDetails) => {
+                    movie = movieDetails;
+                });
+        });
+        beforeEach(() => {
+            cy.visit(`/movies/${movies[1].id}`);
+
+        })
+        it("navigates to the production company details page and change browser URL", () => {
+            cy.get("li").eq(-1).click();
+            cy.url().should("include", `/companies/${movie.production_companies[3].id}`);
+        });
+    });
+
+});
