f99e7674a08faa664b8d5fe5f9dc01dc2b19364b
Yuanhao-Luo <1097308076@qq.com>
2022-11-07 12:29:25 +0000

    test language filter

--- movies/cypress/support/e2e.js
+++ movies/cypress/support/e2e.js
@@ -3,3 +3,6 @@ export const filterByTitle = (movieList, string) =>
 
 export const filterByGenre = (movieList, genreId) =>
     movieList.filter((m) => m.genre_ids.includes(genreId));
+
+export const filterByLanguage = (movieList, language) =>
+    movieList.filter((m) => m.original_language === language)
